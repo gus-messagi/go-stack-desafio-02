b11464392b49bb43796009985346dfcabe4a304d
Gustavo Messagi <gustavo.ramos.messagi@gmail.com>
2020-08-26 18:40:59 -0300

    challenge solved

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,89 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.send(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repo);
+
+  response.send(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const index = repositories.map((element) => element.id).indexOf(id);
+
+  if (index === -1) {
+    response.statusCode = 400;
+    response.send();
+    return;
+  }
+
+  repositories.splice(index[0], 1);
+
+  const repo = {
+    id,
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repo);
+
+  response.send(repo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex(repository => repository.id === id);
+
+  if (index === -1) {
+    response.statusCode = 400;
+    response.send();
+    return;
+  }
+
+  repositories.splice(index, 1);
+
+  response.status(204).send();  
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.map((element) => element.id).indexOf(id);
+
+  if (index === -1) {
+    response.statusCode = 400;
+    response.send();
+    return;
+  }
+
+  const element = repositories.splice(index[0], 1)[0];
+
+  const repo = {
+    id: element.id,
+    title: element.title,
+    url: element.url,
+    techs: element.techs,
+    likes: element.likes + 1
+  };
+
+  repositories.push(repo);
+
+  response.send(repo);
 });
 
 module.exports = app;
